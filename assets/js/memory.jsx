34ab9b6130eada55f9bdf36afc8cc0c712d3b28b
Alec <alecdifederico@gmail.com>
2018-09-26 23:50:29 -0400

    finished memory

--- assets/js/memory.jsx
+++ assets/js/memory.jsx
@@ -13,8 +13,8 @@ class Memory extends React.Component {
       numMatches: 0,
       numClicks: 0,
       score: 0,
-      firstSelected: null,
-      secondSelected: null,
+      first: null,
+      second: null,
       cards: this.initializeCards()
     };
   }
@@ -30,37 +30,75 @@ class Memory extends React.Component {
     return cards;
   }
 
-  // Flips a card
-  cardClicked(card) {
-    let newState = this.state;
-
-    // Check if the card is the first selected
-    if (!this.state.firstSelected) {
-      newState = _.assign({}, newState, {firstSelected: card});
+  // Checks for a match
+  checkMatch() {
+    let newCard = {};
+    if (this.state.first.letter == this.state.second.letter) {
+      newCard = {isMatched: true, color: 'green'};
+      let newState = _.assign({}, this.state, {numMatches: this.state.numMatches + 1});
+      this.setState(newState);
+    } else {
+      newCard = {isFlipped: false, color: 'gray'};
     }
 
-    // Check if the card is the second selected
-    if (!this.state.secondSelected && this.state.firstSelected) {
-      newState = _.assign({}, newState, {secondSelected: card});
-    }
-
-    // Compare them
-  
-    let updatedCards = _.map(this.state.cards, (unflipped) => {
-      if (unflipped.key == card.key) {
-        return _.extend(unflipped, {isFlipped: true, color: 'yellow'});
+    let updatedCards = _.map(this.state.cards, (card) => {
+      if (card.key == this.state.first.key || card.key == this.state.second.key) {
+        return _.extend(card, newCard);
       } else {
-        return unflipped;
+        return card;
       }
     });
 
-    // Create the new state
-    newState = _.assign({}, newState, {
-      numClicks: this.state.numClicks + 1,
+    let newState = _.assign({}, this.state, {
+      first: null,
+      second: null,
       cards: updatedCards
     });
-
     this.setState(newState);
+  }
+
+  // Flips a card
+  cardClicked(card) {
+    // update card to correct color
+    if (this.state.second || card.isMatched) {
+      return;
+    } else {
+      let updatedCards = _.map(this.state.cards, (unflipped) => {
+        if (unflipped.key == card.key) {
+          return _.extend(unflipped, {isFlipped: true, color: 'yellow'});
+        } else {
+          return unflipped;
+        }
+      });
+  
+      let newState = _.assign({}, this.state, {
+        cards: updatedCards
+      });
+      this.setState(newState);
+    }
+
+    // Check for first card
+    if (!this.state.first) {
+      let newState = _.assign({}, this.state, {
+        first: card,
+        numClicks: this.state.numClicks + 1
+      });
+      this.setState(newState);
+      return;
+    }
+
+    // Check for second card
+    if (!this.state.second && this.state.first != card) {
+      let newState = _.assign({}, this.state, {
+        second: card,
+        numClicks: this.state.numClicks + 1
+      });
+      this.setState(newState);
+      setTimeout(() => {
+        this.checkMatch();
+      }, 2000);
+    }
+    
   }
 
   // Resets the game
@@ -69,14 +107,15 @@ class Memory extends React.Component {
       numMatches: 0,
       numClicks: 0,
       score: 0,
-      firstSelected: null,
-      secondSelected: null,
+      first: null,
+      second: null,
       cards: this.initializeCards()
     });
 
     this.setState(cleanState);
   }
 
+  // Render the game
   render() {
     let title = <div className="column">
       <h1>Memory Game</h1>
@@ -92,7 +131,16 @@ class Memory extends React.Component {
           {title}
         </div>
         <div className="row">
-          {cards}
+          {cards.slice(0, 4)}
+        </div>
+        <div className="row">
+          {cards.slice(4, 8)}
+        </div>
+        <div className="row">
+          {cards.slice(8, 12)}
+        </div>
+        <div className="row">
+          {cards.slice(12, 16)}
         </div>
         <div className="row">
           <div className="column column-50">
@@ -110,7 +158,7 @@ class Memory extends React.Component {
 // Renders a card
 function Card(props) {
   let card = props.card;
-  let value = '?';
+  let value = '?'; // ?
 
   if (card.isFlipped) {
     value = card.letter;
